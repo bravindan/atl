ba4a126d5eeae1a06631f03f781606053e2de910
bravindan <danielbravin0@gmail.com>
2026-02-19 18:05:03 +0300

    Initial project setup

---
+++ src/main/java/atl/atl/AtlApplication.java
@@ -0,0 +1,13 @@
+package atl.atl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlApplication.class, args);
+	}
+
+}
